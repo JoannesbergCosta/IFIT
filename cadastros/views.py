e99f2244daea78e8c1e38e2d63778a07dc818447
joannesbergCosta <joannesberg.100@gmail.com>
2024-10-25 19:59:02 -0300

    Controle de acesso por login (permitir só usuários autenticados)  maneira de como bloquear certas páginas para visitantes e permitir que apenas usuários autenticados (logados) possam acessar determinadas páginas.

--- cadastros/views.py
+++ cadastros/views.py
@@ -4,90 +4,108 @@ from .models import Campo, User, Exercicio, TrainingExercicio
 from django.urls import reverse_lazy
 from .forms import TrainingExercicioForm
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 # Create Views
-class CampoCreate(CreateView):
+class CampoCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Campo
     fields = ['nome']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campos')
 
-class UserCreate(CreateView):
+class UserCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = User
     fields = ['matricula', 'campo', 'nome']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-users')
 
-class ExercicioCreate(CreateView):
+class ExercicioCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = Exercicio
     fields = ['exercicio', 'tipo', 'grupo', 'descricao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-exercicios')
 
-class TrainingExercicioCreate(CreateView):
+class TrainingExercicioCreate(LoginRequiredMixin, CreateView):
+    login_url = reverse_lazy('login')
     model = TrainingExercicio
     form_class = TrainingExercicioForm
     template_name = 'cadastros/form_training_exercicio.html'
     success_url = reverse_lazy('listar-training-exercicios')
 
 # Update Views
-class CampoUpdate(UpdateView):
+class CampoUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Campo
     fields = ['nome']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-campos')
 
-class UserUpdate(UpdateView):
+class UserUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = User
     fields = ['matricula', 'campo', 'nome']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-users')
 
-class ExercicioUpdate(UpdateView):
+class ExercicioUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = Exercicio
     fields = ['exercicio', 'tipo', 'grupo', 'descricao']
     template_name = 'cadastros/form.html'
     success_url = reverse_lazy('listar-exercicios')
 
-class TrainingExercicioUpdate(UpdateView):
+class TrainingExercicioUpdate(LoginRequiredMixin, UpdateView):
+    login_url = reverse_lazy('login')
     model = TrainingExercicio
     form_class = TrainingExercicioForm
     template_name = 'cadastros/form_training_exercicio.html'
     success_url = reverse_lazy('listar-training-exercicios')
 
 # Delete Views
-class CampoDelete(DeleteView):
+class CampoDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Campo
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-campos')
 
-class UserDelete(DeleteView):
+class UserDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = User
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-users')
 
-class ExercicioDelete(DeleteView):
+class ExercicioDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = Exercicio
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-exercicios')
 
-class TrainingExercicioDelete(DeleteView):
+class TrainingExercicioDelete(LoginRequiredMixin, DeleteView):
+    login_url = reverse_lazy('login')
     model = TrainingExercicio
     template_name = 'cadastros/form-excluir.html'
     success_url = reverse_lazy('listar-training-exercicios')
 
 # List Views
-class CampoList(ListView):
+class CampoList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Campo
     template_name = 'cadastros/listas/campo.html'
 
-class UserList(ListView):
+class UserList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = User
     template_name = 'cadastros/listas/user.html'
 
-class ExercicioList(ListView):
+class ExercicioList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = Exercicio
     template_name = 'cadastros/listas/exercicio.html'
 
-class TrainingExercicioList(ListView):
+class TrainingExercicioList(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('login')
     model = TrainingExercicio
     template_name = 'cadastros/listas/training_exercicio.html'
