97827ff71f54fc68410e6a0738e0150c20e61d5b
joannesbergCosta <joannesberg.100@gmail.com>
2024-11-20 10:31:21 -0300

    Criar um perfil de usuário com Matricula, Nome e Email.

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class Perfil(models.Model):
+    nome_completo = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=100, null=True)
+    matricula = models.CharField(max_length=14, null=True)
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,12 @@
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
 from django.views.generic.edit import CreateView
 from django.contrib.auth.models import User
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
+
+from .models import Perfil
 
 from django.shortcuts import render
 from django.contrib.auth import logout
@@ -16,6 +21,19 @@ class UsuarioCreate(CreateView):
     template_name = "cadastros/form.html"
     form_class = UsuarioForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+
+        grupo = get_object_or_404(Group, name="Docente")
+
+        url = super().form_valid(form)
+
+        self.object.groups.add(grupo)
+        self.object.save()
+
+        Perfil.objects.create(usuario=self.object)
+
+        return url
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
